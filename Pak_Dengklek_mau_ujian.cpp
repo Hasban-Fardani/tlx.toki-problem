0a2b32205dfa2d0f8dca4f7a09ead87efc9c117e
hasban-fardani <RemajaNgodingChannel@gmail.com>
2022-01-04 03:33:23 +0700

    first commit

---
+++ Pak_Dengklek_mau_ujian.cpp
@@ -0,0 +1,17 @@
+/*
+soal : Pak Dengklek mau ujian
+link : https://tlx.toki.id/problems/ngoding-seru-2015-oct-pemula/B
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n >= 0 and n <= 100){
+        cout << "YA" << endl;
+    } else {
+        cout << "TIDAK" << endl;
+    }
+}
